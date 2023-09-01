30a1d0744a9a8f0d693a9a65005f2723be1fca56
IT <mr.marvel.christevan@gmail.com>
2023-09-01 13:07:41 +0700

    fix server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Yajra\DataTables\Html\Builder;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Builder::useVite();
+        // Builder::useVite();
+        Schema::defaultStringLength(191);
     }
 }
