2cd178fb2aff07cef760e9edaaa3868b2ba2bf28
IT <mr.marvel.christevan@gmail.com>
2023-09-12 18:08:49 +0700

    set timezone

--- resources/views/pages/company/index.blade.php
+++ resources/views/pages/company/index.blade.php
@@ -6,15 +6,12 @@
         <div class="page-block">
             <div class="row align-items-center">
                 <div class="col-md-12">
-                    <div class="page-header-title">
-                        <h5 class="m-b-10">Publishers</h5>
-                    </div>
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item">
-                            <a href="{{url('/home')}}">Home</a>
+                            <a href="{{url('/home')}}">Dashboard</a>
                         </li>
                         <li class="breadcrumb-item" aria-current="page">
-                            Index
+                            Utama
                         </li>
                     </ul>
                 </div>
